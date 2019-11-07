86d5fab176540a424be5fcab30a0e413340eb071
ylouazel <yoann.louazel@iutrs.unistra.fr>
2019-11-07 22:21:04 +0100

    fix bug dans la fonction courbe2 de la classe taux

--- src/Model/taux.java
+++ src/Model/taux.java
@@ -1,37 +1,47 @@
 package Model;
 
+import java.math.BigDecimal;
 import java.util.Map;
 
 public class taux implements AbstractFormula {
 
     @Override
     public void updateByOneLever(Indicator indicator, Lever lever) {
-        updateByLevers(indicator);
+
     }
 
     @Override
     public void updateByLevers(Indicator indicator) {
         indicator.setValue(0);
         long valMap;
+        System.out.println("#####################################");
+        System.out.println("nom indicateur : "+ indicator.get_name());
         for (Map.Entry mapEntry : indicator.getFormula().entrySet()) {
+            System.out.println("# # # # # # # # # # # # # # # # # # #");
             valMap = ((AbstractElement) mapEntry.getKey()).getValue();
             if (mapEntry.getValue() == "20c1") {
                 indicator.setValue(indicator.getValue() + courbe1(valMap,0.2));
+                System.out.println(((AbstractElement) mapEntry.getKey()).get_name() +" coeff 0.2 courbe 1 val : "+ valMap + "\n val courbe : "+courbe1(valMap,0.2));
             }
             else if (mapEntry.getValue() == "40c2") {
                 indicator.setValue(indicator.getValue() + courbe2(valMap,0.4));
+                //System.out.println(((AbstractElement) mapEntry.getKey()).get_name() + " coeff 0.4 courbe 2 val : "+ valMap + "\n val courbe : "+courbe2(valMap,0.4));
             }
             else if (mapEntry.getValue() == "40/100") {
                     indicator.setValue(indicator.getValue() + (long) (valMap * 0.4));
+                System.out.println(((AbstractElement) mapEntry.getKey()).get_name() +" coeff 40/100 val : "+ (valMap * 0.4)+" "+valMap);
                 }
             else if (mapEntry.getValue() == "30/100") {
                 indicator.setValue(indicator.getValue() + (long) (valMap * 0.3));
+                System.out.println(((AbstractElement) mapEntry.getKey()).get_name() +" coeff 30/100 val : "+ (valMap * 0.3)+" "+valMap);
                 }
             else if (mapEntry.getValue() == "35c2") {
                 indicator.setValue(indicator.getValue() + courbe2(valMap,0.35));
+                System.out.println(((AbstractElement) mapEntry.getKey()).get_name() +" coeff 0.35 courbe 2 val : "+ valMap + "\n val courbe : "+courbe2(valMap,0.35));
                 }
             else if (mapEntry.getValue() == "35c3") {
                 indicator.setValue(indicator.getValue() + courbe3(valMap,0.35));
+                System.out.println(((AbstractElement) mapEntry.getKey()).get_name() +" coeff 0.35 courbe 3 val : "+ valMap + "\n val courbe : "+courbe3(valMap,0.35));
                 }
             }
 
@@ -40,6 +50,7 @@ public class taux implements AbstractFormula {
             } else if (indicator.getValue() > 100) {
                 indicator.setValue(100);
             }
+        System.out.println("#####################################");
         }
 
     public static long courbe1(long value,double coef){
@@ -68,12 +79,17 @@ public class taux implements AbstractFormula {
         }
         else if (value < 300000) {
             valCourbe = (long) (((66.6666 / 1000000) * value) * coef);
+            System.out.println("1");
         } else if (value < 500000) {
-            valCourbe = (long) ((((300 / 1000000) * value) - 70) * coef);
+            BigDecimal frac = new BigDecimal(300);
+            frac = frac.divide(new BigDecimal(1000000));
+            valCourbe = frac.multiply(new BigDecimal(value)).subtract(new BigDecimal(70)).multiply(new BigDecimal(coef)).longValue();
         } else if (value < 700000) {
             valCourbe = (long) ((((87.5 / 1000000) * value) + 36.25) * coef);
+            System.out.println("3");
         } else if (value < 1000000) {
             valCourbe = (long) ((((8.33 / 1000000) * value) + 91.666) * coef);
+            System.out.println("4");
         }
         else {
             valCourbe = (long)(coef * 100);
