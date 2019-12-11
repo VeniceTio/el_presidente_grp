fcd4e7fc1d204aaad483eafe60f8c84265fa64f0
Yoann067 <yoann.louazel@gmail.com>
2019-12-11 12:43:48 +0100

    affinage satisfaction

--- src/model/PourcentSatisfaction.java
+++ src/model/PourcentSatisfaction.java
@@ -83,25 +83,35 @@ public class PourcentSatisfaction implements AbstractFormula {
     @Override
     public void updateByLevers(Indicator indicator) {
         indicator.setValue(0);
+        System.out.println("################## voila regarde quoi #################");
+        System.out.println("#    "+indicator.toString());
         for (Map.Entry mapEntry : indicator.getFormula().entrySet()) {
-            //System.out.println(mapEntry);
+            System.out.println("###################");
+            System.out.println("#    "+mapEntry);
             if (mapEntry.getValue()=="r"){
                 long val = ElementControl.getInstance().getElement("nombre d'étudiant").getValue();
                 indicator.setValue(indicator.getValue() + (int)(courbe1(((AbstractElement)mapEntry.getKey()).getValue()/val)*0.3333));
+                System.out.println("# courbe 1 : "+ (courbe1(((AbstractElement)mapEntry.getKey()).getValue()/val)*0.3333));
             } else if(mapEntry.getValue() =="3"){
                 indicator.setValue(indicator.getValue() + (long)(((AbstractElement)mapEntry.getKey()).getValue()*0.3333));
-            } else if(mapEntry.getValue() =="c35"){
-                indicator.setValue(indicator.getValue() + (long)(courbe3(((AbstractElement)mapEntry.getKey()).getValue())*0.5));
+                System.out.println("# courbe 1 : "+(((AbstractElement)mapEntry.getKey()).getValue()*0.3333));
+            } else if(mapEntry.getValue() =="c34"){
+                indicator.setValue(indicator.getValue() + (long)(courbe3(((AbstractElement)mapEntry.getKey()).getValue())*0.4));
+                System.out.println("# courbe 1 : "+(long)(courbe3(((AbstractElement)mapEntry.getKey()).getValue())*0.5));
             } else if(mapEntry.getValue() =="c33"){
                 indicator.setValue(indicator.getValue() + (long)(courbe3(((AbstractElement)mapEntry.getKey()).getValue())*0.333));
-            } else if (mapEntry.getValue() =="25"){
+                System.out.println("# courbe 1 : "+(courbe3(((AbstractElement)mapEntry.getKey()).getValue())*0.333));
+            } else if (mapEntry.getValue() =="c26"){
                 long rapport = ((AbstractElement) mapEntry.getKey()).getValue();
-                System.out.println("##########");
-                System.out.println("#   "+rapport+"   #");
-                System.out.println("##########");
-                indicator.setValue(indicator.getValue() + (long)(courbe2(rapport)*0.5));
+                //System.out.println("##########");
+                //System.out.println("#   "+rapport+"   #");
+                //System.out.println("##########");
+                indicator.setValue(indicator.getValue() + (long)(courbe2(rapport)*0.6));
+                System.out.println("# courbe 1 : "+(courbe2(rapport)*0.5));
             }
         }
+        System.out.println("#    "+indicator.toString());
+        System.out.println("################## c'est fini quoi #################");
         if (indicator.getValue()>100){
             indicator.setValue(100);
         }
@@ -111,7 +121,7 @@ public class PourcentSatisfaction implements AbstractFormula {
     }
 
     /**
-     * A=(0,-50),B=(100,10),C=(200,40),D=(300,70)
+     * A=(0,0),B=(100,10),C=(200,40),D=(300,70)
      * E=(600,100)
      * @param value
      * @return
@@ -119,9 +129,9 @@ public class PourcentSatisfaction implements AbstractFormula {
     public static int courbe1(long value){
         int valCourbe=0;
         if (value<0){
-            valCourbe = -50;
+            valCourbe = -10;
         } else if (value <=100){
-            valCourbe = (int)(0.6*value-50);
+            valCourbe = (int)(0.1*value);
         } else if (value <=300){
             valCourbe = (int)(0.3*value-20);
         } else if (value <= 600){
@@ -161,16 +171,16 @@ public class PourcentSatisfaction implements AbstractFormula {
     }
 
     /**
-     * A=(0,-80),B=(40,10),C=(50,40),D=(80,90),E=(100,100)
+     * A=(0,-30),B=(40,10),C=(50,40),D=(80,90),E=(100,100)
      * @param value
      * @return
      */
     public static int courbe3(long value){
         int valCourbe=0;
         if (value<0){
-            valCourbe = -80;
+            valCourbe = -30;
         } else if (value <=40){
-            valCourbe = (int)(2.25*value-80);
+            valCourbe = (int)(0.6*value-20);
         } else if (value <=50){
             valCourbe = (int)(3*value-110);
         } else if (value <= 80){
