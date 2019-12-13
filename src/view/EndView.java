eaf083d6e60fd9ee2b92ccd75c15f81ea44bdec3
Tony Le <tle@unistra.fr>
2019-12-13 10:43:54 +0100

    Modification du contenu des JSON + copie des noms d'indicateurs dans une liste + transfert methode graphique dans classe graphique

--- src/view/EndView.java
+++ src/view/EndView.java
@@ -20,7 +20,7 @@ import model.IndicatorType;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-
+import view.Graphic;
 public class EndView {
     /**
      * Instance de la vue
@@ -39,7 +39,6 @@ public class EndView {
     }
 
     ElementControl ec = ElementControl.getInstance();
-
     /**
      * Méthode permettant de construire la vue puis l'afficher
      */
@@ -57,14 +56,14 @@ public class EndView {
         AnchorPane buttonsPane = (AnchorPane) container.getChildren().get(0); // AnchorPane id "button-pane"
         AnchorPane graphicPane = (AnchorPane) container.getChildren().get(1); // AnchorPane id "graphic-pane"
 
-
         //Évenemment permettant l'affichage d'un grapique pour un indicateur donné en paramètre
         EventHandler<ActionEvent> individualGraphicButton = event -> {
             graphicPane.getChildren().clear();
 
             String indicatorGroup = ((Button) event.getSource()).getText();
 
-            LineChart<Integer, Long> iChart = getIndicatorGraphic(indicatorGroup.toLowerCase());
+            Graphic g = new Graphic();
+            LineChart<Integer, Long> iChart = g.getIndicatorGraphic(indicatorGroup.toLowerCase());
 
             iChart.setPrefWidth(graphicPane.getWidth() - 100);
             iChart.setPrefHeight(graphicPane.getHeight() - 50);
@@ -82,7 +81,8 @@ public class EndView {
 
             String indicatorName = ((Button) event.getSource()).getText();
 
-            LineChart<Integer, Long> iChart = getIndicatorsGraphic(indicatorName.toLowerCase());
+            Graphic g = new Graphic();
+            LineChart<Integer, Long> iChart = g.getIndicatorsGraphic(indicatorName.toLowerCase());
 
             iChart.setPrefWidth(graphicPane.getWidth() - 100);
             iChart.setPrefHeight(graphicPane.getHeight() - 50);
@@ -94,21 +94,16 @@ public class EndView {
             event.consume();
         };
 
-        String[] indicators = {"Satisfaction étudiante",
-                "Nombre d'étudiants", "Taux de réussite",
-                "Recherche fondamentale", "Nombre d'articles publiés",
-                "Réputation de la recherche", "Qualité des formations",
-                "État des batiments", "Satisfaction professeurs",
-                "Nombre de professeurs", "Taux d'insertion pro.",
-                "Recherche appliquée", "Nombre de prix nobel",
-                "Réputation des formations", "Argent disponible"};
-
-        ArrayList<String> indicatorButton = new ArrayList<>(Arrays.asList(indicators));
+        ArrayList<String> indicators = new ArrayList<>();
+        for(Indicator indic : ec.getIndicators()) {
+            String indicatorName = indic.get_name();
+            indicators.add(indicatorName.substring(0, 1).toUpperCase() + indicatorName.substring(1));
+        }
 
         for(Node n : buttonsPane.getChildren()) {
             if(n instanceof Button) {
                 Button b = (Button) n;
-                if(indicatorButton.contains(b.getText())) {
+                if(indicators.contains(b.getText())) {
                     b.setOnAction(individualGraphicButton);
                 }
                 else {
@@ -121,141 +116,5 @@ public class EndView {
         stage.setScene(new Scene(p));
         stage.setFullScreen(true);
         stage.show();
-    }
-
-    /**
-     * Méthode construisant un graphique de type "LineChart"
-     * d'un graphique puis le renvoie
-     * @param name l'indicateur sur lequel on créer le graphe
-     * @return le graphe de l'indicateur
-     */
-    private LineChart<Integer, Long> getIndicatorGraphic(String name) {
-        ArrayList<Indicator> indicatorList = ec.getIndicators();
-        int indicatorIndex = -1;
-
-        for (int i = 0; i < indicatorList.size(); i++) {
-            Indicator indic = indicatorList.get(i);
-            if (indic.get_name().equals(name)) {
-                indicatorIndex = i;
-            }
-        }
-
-        ArrayList<Long> indicatorHistory = indicatorList.get(indicatorIndex).get_history();
-
-        Long maxValue =  indicatorHistory.get(0);
-        for (Long tempValue : indicatorHistory) {
-            maxValue = Math.max(tempValue, maxValue);
-        }
-
-        int tickValue = Math.toIntExact(maxValue / 10);
-        int upperBoundY = Math.toIntExact(maxValue + tickValue);
-
-        String indicatorType = "";
-        if(indicatorList.get(indicatorIndex).getType().equals(IndicatorType.PERCENTAGE)) {
-            indicatorType = "(%)";
-            upperBoundY = 100;
-            tickValue = 5;
-        }
-
-        NumberAxis xAxis = new NumberAxis(0, indicatorHistory.size() -1, 1);
-        xAxis.setLabel("Semestre");
-
-        System.out.println(indicatorHistory.size());
-
-        NumberAxis yAxis = new NumberAxis(0, upperBoundY, tickValue);
-        yAxis.setLabel("Valeur de l'indicateur" + indicatorType);
-
-        LineChart<Integer, Long> indicatorChart = new LineChart(xAxis, yAxis);
-        XYChart.Series<Integer, Long> series = new XYChart.Series<>();
-
-        String displayName = name.substring(0, 1).toUpperCase() + name.substring(1);
-        series.setName(displayName);
-
-        for(int i = 0; i < indicatorHistory.size(); i++) {
-            series.getData().add(new XYChart.Data<>(i, indicatorHistory.get(i)));
-        }
-
-        indicatorChart.getData().add(series);
-        return indicatorChart;
-    }
-
-    /**
-     * Méthode construisant une graphique de plusieurs graphiques
-     * de type "LineChart" puis le renvoie
-     * @param name le nom de la famille de l'indicateur
-     * @return le graphe des indicateurs
-     */
-    private LineChart<Integer, Long> getIndicatorsGraphic(String name) {
-        ArrayList<Indicator> indicatorList = ec.getIndicators();
-        ArrayList<Integer> indexes = new ArrayList<>();
-
-        for(int i = 0; i < indicatorList.size(); i++) {
-            Indicator indic = indicatorList.get(i);
-            String cutName = name.substring(0, name.length() - 1);
-            String tempName = indic.get_name().substring(0, cutName.length());
-
-            if(cutName.equals(tempName))
-                indexes.add(i);
-        }
-
-        ArrayList<Long> indicatorHistory1 = indicatorList.get(indexes.get(0)).get_history();
-        ArrayList<Long> indicatorHistory2 = indicatorList.get(indexes.get(1)).get_history();
-
-        Long maxValue = indicatorHistory1.get(0);
-        for(Long l1 : indicatorHistory1) {
-            for(Long l2 : indicatorHistory2) {
-                maxValue = Math.max(l1, l2);
-            }
-        }
-
-        int tickValue = Math.toIntExact(maxValue / 10);
-        int upperBoundY = Math.toIntExact(maxValue + tickValue);
-
-        String indicatorType = "";
-
-        IndicatorType iType1 = indicatorList.get(indexes.get(0)).getType();
-        IndicatorType iType2 = indicatorList.get(indexes.get(1)).getType();
-
-        if(iType1.equals(IndicatorType.PERCENTAGE) && iType2.equals(IndicatorType.PERCENTAGE)) {
-            indicatorType = "(%)";
-            upperBoundY = 100;
-            tickValue = 5;
-        }
-
-        int upperBoundX = Math.max(indicatorHistory1.size(), indicatorHistory2.size());
-        NumberAxis xAxis = new NumberAxis(0, upperBoundX - 1, 1);
-        xAxis.setLabel("Semestre");
-
-        NumberAxis yAxis = new NumberAxis(0, upperBoundY, tickValue);
-        yAxis.setLabel("Valeur de l'indicateur" + indicatorType);
-
-        LineChart<Integer, Long> indicatorsChart = new LineChart(xAxis, yAxis);
-
-        XYChart.Series<Integer, Long> series1 = new XYChart.Series<>();
-        XYChart.Series<Integer, Long> series2 = new XYChart.Series<>();
-
-        String tempName1 = indicatorList.get(indexes.get(0)).get_name();
-        String tempName2 = indicatorList.get(indexes.get(1)).get_name();
-
-        String displayName1 = tempName1.substring(0, 1).toUpperCase() + tempName1.substring(1);
-        String displayName2 = tempName1.substring(0, 1).toUpperCase() + tempName2.substring(1);
-
-        series1.setName(displayName1);
-        series2.setName(displayName2);
-
-        int size = indicatorHistory1.size();
-
-        if(indicatorHistory2.size() < size)
-            size = indicatorHistory2.size();
-
-        for(int index = 0; index < size; index++) {
-            series1.getData().add(new XYChart.Data<>(index, indicatorHistory1.get(index)));
-            series2.getData().add(new XYChart.Data<>(index, indicatorHistory2.get(index)));
-        }
-
-        indicatorsChart.getData().add(series1);
-        indicatorsChart.getData().add(series2);
-
-        return indicatorsChart;
     }
 }
